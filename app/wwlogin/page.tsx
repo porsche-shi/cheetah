4910e490975509aaf45dc1586b8c2666b3c32528
porsche <porsche.shi@gmail.com>
2024-09-14 16:41:06 +0800

    dns verify

--- app/wwlogin/page.tsx
+++ app/wwlogin/page.tsx
@@ -1,15 +1,14 @@
-import {useEffect, useState} from "react";
-
-export default function WWLogin() {
-    const [content, setContent] = useState('');
+export default async function WWLogin() {
     const url = "https://login.work.weixin.qq.com/wwlogin/sso/login/?login_type=CorpApp&appid=wwdf51420bb8d44d91&redirect_uri=http%3A%2F%2Fcheetah.tntlinking.com&state=STATE&agentid=1000003"
-    useEffect(() => {
-        fetch(url).then(response => response.text()).then(text => {
-            setContent(text);
-        });
-    }, []);
+    // useEffect(() => {
+    //     fetch(url).then(response => response.text()).then(text => {
+    //         setContent(text);
+    //     });
+    // }, []);
+
+    const resp = await fetch(url)
 
     return (
-        <div dangerouslySetInnerHTML={{__html: content}}></div>
+        <div dangerouslySetInnerHTML={{__html: resp.text()}}></div>
     )
 };
