020e065c6a00657645123c092ae749fba6bab426
porsche <porsche.shi@gmail.com>
2024-09-22 16:50:02 +0800

    autologin

--- app/autologin/verify/page.tsx
+++ app/autologin/verify/page.tsx
@@ -1,3 +1,19 @@
-export default async function Verify({params}: { params: { code: string } }) {
-    return <div>{params.code}</div>
+'use client'
+
+import {useSearchParams} from "next/navigation";
+import {Suspense} from "react";
+
+function Code() {
+    const params = useSearchParams()
+    const code = params.get('code')
+    const state = params.get('state')
+    return <div>code = {code}, state = {state}</div>
+
+}
+export default function Page() {
+    return (
+        <Suspense>
+            <Code />
+        </Suspense>
+    )
 };
