a66092ac4a6cff681de9b65af1bd81f9c558628a
porsche <porsche.shi@gmail.com>
2024-09-22 16:53:08 +0800

    autologin

--- app/autologin/page.tsx
+++ app/autologin/page.tsx
@@ -6,8 +6,8 @@ export default async function AutoLogin() {
     const agent_id = '1000003'
     const redirect_uri = encodeURIComponent('https://cheetah.tntlinking.com/autologin/verify')
     const response_type = 'code'
-    const scope = 'snsapi_base'
-    // const scope = 'snsapi_privateinfo'
+    // const scope = 'snsapi_base'
+    const scope = 'snsapi_privateinfo'
     const state = 'cheetah-web'
 
     redirect(`https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appid}&redirect_uri=${redirect_uri}&response_type=${response_type}&scope=${scope}&state=${state}&agentid=${agent_id}#wechat_redirect`)
