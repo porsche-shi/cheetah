b58180067bc75c9e85b6475a68a2c3083f419cf6
porsche <porsche.shi@gmail.com>
2024-09-25 15:55:39 +0800

    authentication

--- app/autologin/verify/page.tsx
+++ app/autologin/verify/page.tsx
@@ -1,19 +1,25 @@
-'use client'
+'use server'
 
 import {useSearchParams} from "next/navigation";
 import {Suspense} from "react";
 
-function Code() {
-    const params = useSearchParams()
-    const code = params.get('code')
-    const state = params.get('state')
-    return <div>code = {code}, state = {state}</div>
+// function Code() {
+//     const params = useSearchParams()
+//     const code = params.get('code')
+//     const state = params.get('state')
+//     return <div>code = {code}, state = {state}</div>
+//
+// }
+// export default function Page() {
+//     return (
+//         <Suspense>
+//             <Code />
+//         </Suspense>
+//     )
+// };
 
-}
-export default function Page() {
+export default async function Page({searchParams}: { searchParams: any }) {
     return (
-        <Suspense>
-            <Code />
-        </Suspense>
+        <div>查询参数是: {searchParams}</div>
     )
 };
